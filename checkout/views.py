89f96352163fde54e6d85ef722e88dbf24dabc78
Chris Mecrow <mecrow@live.co.uk>
2022-06-28 09:21:24 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,8 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LFaNIBz28O6pWUISi9TS4UpSK6gzHQ6RVXDogvr4NknW0YcoAdplp1lSC6QHdHO8dvbfCXWQ3enWC3z4BemeCDd00TFgjRLnC',
     }
 
     return render(request, template, context)
